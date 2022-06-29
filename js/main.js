c423220cf378a78e449f719a30784baa3de933d1
Luciano <luc.merce@hotmail.com>
2022-06-29 14:23:55 -0300

    Reaproveitando código

--- js/main.js
+++ js/main.js
@@ -2,18 +2,35 @@ const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
+const controle = document.querySelectorAll(".controle-ajuste");
 
-somar.addEventListener("click", (evento) => {
 
-    braco.value = parseInt(braco.value) + 1;
+somar.addEventListener("click", () => {
+
+    manipulaDados("somar");
 
 });
 
-subtrair.addEventListener("click", (evento) => {
+subtrair.addEventListener("click", () => {
+
+        manipulaDados("subtrair");
+
+});
+
+
+function manipulaDados (operacao) {
+
+    if (operacao === "subtrair") {
 
         braco.value = parseInt(braco.value) - 1;
 
-});
+    } else {
+
+        braco.value = parseInt(braco.value) + 1;
+        
+    }
+
+}
 
 
 /*
