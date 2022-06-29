33d0fb9439a6a44141b5c56a0275f3ed29a67ac6
Luciano <luc.merce@hotmail.com>
2022-06-29 14:15:54 -0300

    Alterando um item na página

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+
+somar.addEventListener("click", (evento) => {
+
+    braco.value = parseInt(braco.value) + 1;
+
+});
+
+subtrair.addEventListener("click", (evento) => {
+
+        braco.value = parseInt(braco.value) - 1;
+
+});
+
+
+/*
 const robotron = document.querySelector("#robotron");
 
 robotron.addEventListener("click", (evento) => {
@@ -13,3 +32,4 @@ function dizOi(nome) {
 }
 
 dizOi("Pedro");
+*/
